716e6ebf181041b51221d573f8f544058075902c
nishant-raj1 <91952661+nishant-raj1@users.noreply.github.com>
2021-10-12 09:39:27 +0530

    Add files via upload
    Java files of the code (Account types)

---
+++ CreditEnquiryTest.java
@@ -0,0 +1,12 @@
+package sample;
+
+//This program tests class CreditInquiry.
+
+public class CreditEnquiryTest
+{  
+public static void main( String args[] )
+{
+   CreditEniquiry application = new CreditEniquiry();
+   application.processRequests();
+} // end main
+} // end class CreditInquiryTest
